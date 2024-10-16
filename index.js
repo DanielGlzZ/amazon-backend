8cf7ce8d8f4c9fc7a53f91621ee2045e21bac408
Jorge Reyes <rjorge99@hotmail.com>
2024-10-16 15:29:53 -0600

    se agrega el servidor en node para hacer las conexiones

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios'); // Importa Axios
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json()); // Para manejar JSON en el cuerpo de las solicitudes
+
+// Ruta para obtener participaciones del marketplace
+app.get('/api/marketplace-participations', async (req, res) => {
+   const token = req.headers['x-amz-access-token']; // Obtén el token del encabezado
+
+   try {
+      const response = await axios.get(
+         'https://sellingpartnerapi-na.amazon.com/sellers/v1/marketplaceParticipations',
+         {
+            headers: {
+               'x-amz-access-token': token,
+               'Content-Type': 'application/json',
+               Accept: 'application/json'
+            }
+         }
+      );
+      res.json(response.data); // Envía la respuesta de la API a tu cliente
+   } catch (error) {
+      console.error('Error al obtener las participaciones:', error);
+      if (error.response) {
+         res.status(error.response.status).json(error.response.data);
+      } else {
+         res.status(500).json({ error: 'Error interno del servidor' });
+      }
+   }
+});
+
+app.listen(PORT, () => {
+   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
